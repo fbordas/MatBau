f7b688b9a24dceadd2d2e15551bc3a585cbfa9dd
Sh1k1 <fbordas@gmail.com>
2016-03-26 15:58:39 -0400

    Solution renaming

---
+++ NanayaSettings/UniversalSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NanayaSettings
+{
+    public class UniversalSettings
+    {
+
+
+    }
+}
